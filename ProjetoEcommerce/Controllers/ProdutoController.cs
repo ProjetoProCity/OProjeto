0be94a019f0f88a410b2ba4364ad0d986e5604ab
tavinzn <otaviosantosandrigo08@gmail.com>
2025-04-10 15:24:18 -0300

    Iniciando as config do ProdutoController

--- ProjetoEcommerce/Controllers/ProdutoController.cs
+++ ProjetoEcommerce/Controllers/ProdutoController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce.Repositorio;
 
 namespace ProjetoEcommerce.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        } 
+
         public IActionResult Index()
+        {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+
+        public IActionResult CadastrarProduto() 
         {
             return View();
         }
