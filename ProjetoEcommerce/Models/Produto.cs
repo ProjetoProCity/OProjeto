d4dae61d086ce11c105006c159fc18832211c8d6
tavinzn <otaviosantosandrigo08@gmail.com>
2025-04-10 14:56:11 -0300

    Criando a model

---
+++ ProjetoEcommerce/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoEcommerce.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Prod { get; set; }
+
+        public string Descr { get; set; }
+        public int Qtd { get; set; }
+        public double Preco { get; set; }
+    }
+}
