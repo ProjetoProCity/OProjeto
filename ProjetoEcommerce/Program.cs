443cae8edfae2fb3defe19c2a05335cdebd12fa9
tavinzn <otaviosantosandrigo08@gmail.com>
2025-04-13 19:12:28 -0300

    Alguns ajustes e adicionando a injeção de dependencia do produto repositorio

--- ProjetoEcommerce/Program.cs
+++ ProjetoEcommerce/Program.cs
@@ -8,8 +8,7 @@ builder.Services.AddControllersWithViews();
 // INJEÇÃO DE DEPENDENCIA 
 builder.Services.AddScoped<UsuarioRepositorio>();
 builder.Services.AddScoped<ClienteRepositorio>();
-
-
+builder.Services.AddScoped<ProdutoRepositorio>();
 
 
 
